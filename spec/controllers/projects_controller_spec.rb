3f3524e128b74b05158a7e1c0a1ad4f760349a4e
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-04 16:31:06 +0600

    Added project_controller_spec. Step4

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,120 +2,116 @@ require 'spec_helper'
 
 describe ProjectsController do
 
-  describe 'index action' do
+  # Не авторизованный пользователь
+  context 'when user not logged in' do
+    describe 'GET #index' do
+      it 'redirects to login page' do
+        get :index
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 
-    # Не авторизованный пользователь
-    context 'when user not logged in' do
-      describe 'GET #index' do
-        it 'redirects to login page' do
-          get :index
-          expect(response).to redirect_to new_user_session_path
+  # Авторизованный пользователь
+  context 'when user logged in' do
+    let(:user) { FactoryGirl.create(:user) }
+    subject { FactoryGirl.create(:project, owner: user) }
+
+    before do
+      sign_in user
+    end
+
+    describe 'GET #index' do
+      it 'renders :index view' do
+        get :index
+        expect(response).to render_template :index
+      end
+
+      it 'assigns the requested project to subject' do
+        get :index
+        expect(assigns(:projects)).to eq([subject])
+      end
+    end
+
+    describe 'GET #show' do
+      it 'renders :show view' do
+        get :show, id: subject
+        expect(response).to render_template :show
+      end
+
+      it 'assigns the requested project to subject' do
+        get :show, id: subject
+        expect(assigns(:projects)).to eq(subject)
+      end
+    end
+
+    describe 'GET #new' do
+      it 'renders :new view' do
+        get :new
+        expect(response).to render_template :new
+      end
+
+      it 'assigns the requested project to new record' do
+        get :new
+        expect(assigns(:projects)).to be_new_record
+      end
+    end
+
+    describe 'POST #create' do
+      context 'with valid attributes' do
+        it 'create new object' do
+          expect{
+            post :create, project: FactoryGirl.attributes_for(:project)
+          }.to change(Project, :count).by(1)
+        end
+
+        it 'redirects to index path' do
+          post :create, project: FactoryGirl.attributes_for(:project)
+          expect(response).to redirect_to projects_path
         end
       end
     end
 
-    # Авторизованный пользователь
-    context 'when user logged in' do
-      let(:user) { FactoryGirl.create(:user) }
-      subject { FactoryGirl.create(:project, owner: user) }
-
-      before do
-        sign_in user
+    describe 'GET #edit' do
+      it 'renders :edit view' do
+        get :edit, id: subject
+        expect(response).to render_template :edit
       end
 
-      describe 'GET #index' do
-        it 'renders :index view' do
-          get :index
-          expect(response).to render_template :index
-        end
-
-        it 'assigns the requested project to subject' do
-          get :index
-          expect(assigns(:projects)).to eq([subject])
-        end
+      it 'assigns the requested project to subject' do
+        get :edit, id: subject
+        expect(assigns(:projects)).to eq(subject)
       end
+    end
 
-      describe 'GET #show' do
-        it 'renders :show view' do
-          get :show, id: subject
-          expect(response).to render_template :show
-        end
-
-        it 'assigns the requested project to subject' do
-          get :show, id: subject
-          expect(assigns(:projects)).to eq(subject)
-        end
-      end
-
-      describe 'GET #new' do
-        it 'renders :new view' do
-          get :new
-          expect(response).to render_template :new
-        end
-
-        it 'assigns the requested project to new record' do
-          get :new
-          expect(assigns(:projects)).to be_new_record
-        end
-      end
-
-      describe 'POST #create' do
-        context 'with valid attributes' do
-          it 'create new object' do
-            expect{
-              post :create, project: FactoryGirl.attributes_for(:project)
-            }.to change(Project, :count).by(1)
-          end
-
-          it 'redirects to index path' do
-            post :create, project: FactoryGirl.attributes_for(:project)
-            expect(response).to redirect_to projects_path
-          end
-        end
-      end
-
-      describe 'GET #edit' do
-        it 'renders :edit view' do
-          get :edit, id: subject
-          expect(response).to render_template :edit
-        end
-
-        it 'assigns the requested project to subject' do
-          get :edit, id: subject
-          expect(assigns(:projects)).to eq(subject)
-        end
-      end
-
-      describe 'PATCH #update' do
-        context 'with valid attributes' do
-          it 'updates object' do
-            expect{
-              post :update, id: subject, project: { title: 'new_project' }
-            }.to change{ subject.reload.title }.to('new_project')
-          end
-
-          it 'redirects to index path' do
-            post :update, id: subject, project: { title: 'new_project' }
-            expect(response).to redirect_to projects_path
-          end
-        end
-      end
-
-      describe 'DELETE #destroy' do
-        before(:each) { @project = FactoryGirl.create :project, owner: user }
-
-        it 'deletes the object' do
+    describe 'PATCH #update' do
+      context 'with valid attributes' do
+        it 'updates object' do
           expect{
-            delete :destroy, id: @project
-          }.to change(Project, :count).by(-1)
+            post :update, id: subject, project: { title: 'new_project' }
+          }.to change{ subject.reload.title }.to('new_project')
         end
 
         it 'redirects to index path' do
-          delete :destroy, id: @project
-          expect(response).to redirect_to projects_path
+          post :update, id: subject, project: { title: 'new_project' }
+          expect(response).to redirect_to project_path(subject)
         end
       end
+    end
 
+    describe 'DELETE #destroy' do
+      before(:each) { @project = FactoryGirl.create :project, owner: user }
+
+      it 'deletes the object' do
+        expect{
+          delete :destroy, id: @project
+        }.to change(Project, :count).by(-1)
+      end
+
+      it 'redirects to index path' do
+        delete :destroy, id: @project
+        expect(response).to redirect_to projects_path
+      end
     end
 
   end
