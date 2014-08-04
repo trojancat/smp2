6071c09e751e6924a056ae969c51e2cb2ff53986
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-04 12:45:31 +0600

    Added separate layout for devise views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  # Страница со списком пользователей
+  def index
+  end
+
+  # Страница просмотра данных пользователя
+  def show
+  end
+
+  # Страница изменения данных пользователя
+  def edit
+  end
+
+  # Обновить данные пользователя
+  def update
+  end
+
+  # Удалить пользователя
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  resources :users, except: [:new, :create]
+
   root 'site#index'
 
   # Example of regular route:
