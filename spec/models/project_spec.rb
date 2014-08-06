94c51804d8aef6bfeafc9169d8f20f6725162c31
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-06 22:35:37 +0600

    Added Project's model validations test. Step2

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,10 +9,9 @@ describe Project do
     expect(@p).to be_valid
   end
 
-  #it { is_expected.to belong_to(:owner).class_name(User) }
-  #it { is_expected.to ensure_length_of(:title).is_at_most(255) }
-  #it { is_expected.to validate_presence_of(:owner) }
-  #it { is_expected.to validate_presence_of(:title) }
-  #it { is_expected.to validate_inclusion_of(:status).in_array([Project::OPENED, Project::CLOSED]) }
-  #it { is_expected.to validate_inclusion_of(:status).in_array([1, 2]) }
+  it { is_expected.to belong_to(:owner).class_name(User) }
+  it { is_expected.to ensure_length_of(:title).is_at_most(255) }
+  it { is_expected.to validate_presence_of(:owner) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_inclusion_of(:status).in_array(Project::status.values) }
 end
