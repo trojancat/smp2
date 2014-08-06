812e76c5e287f05825699b408cd21f683827674e
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-07 00:13:25 +0600

    Added Message's model validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ActiveRecord::Base
+
   belongs_to  :sender, class_name: User
   belongs_to  :acceptor, class_name: User
+
+  # Статусы
+  extend Enumerize
+  enumerize :status, in: {
+      'новое' => 1,
+      'прочитанное' => 2,
+  }
+
+  # Валидация
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :sender, :acceptor, :body, presence: true
+  validates :status, presence: true, inclusion: { in: self.status.values }
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -8,4 +8,13 @@ describe Message do
   it "has a valid factory" do
     expect(@m).to be_valid
   end
+
+  it { should belong_to(:sender).class_name(User) }
+  it { should belong_to(:acceptor).class_name(User) }
+  it { should ensure_length_of(:title).is_at_most(255) }
+  it { should validate_presence_of(:sender) }
+  it { should validate_presence_of(:acceptor) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should ensure_inclusion_of(:status).in_array(Message::status.values) }
 end
