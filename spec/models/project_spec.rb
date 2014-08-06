c78654b62bb3ce1bb5ca59f57ce6688364223141
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-06 23:29:24 +0600

    Added new shoulda-matchers sintax for project_spec.rb. Step2

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,9 +9,9 @@ describe Project do
     expect(@p).to be_valid
   end
 
-  it { is_expected.to belong_to(:owner).class_name(User) }
-  it { is_expected.to ensure_length_of(:title).is_at_most(255) }
-  it { is_expected.to validate_presence_of(:owner) }
-  it { is_expected.to validate_presence_of(:title) }
+  it { should belong_to(:owner).class_name(User) }
+  it { should ensure_length_of(:title).is_at_most(255) }
+  it { should validate_presence_of(:owner) }
+  it { should validate_presence_of(:title) }
   it { should ensure_inclusion_of(:status).in_array(Project::status.values) }
 end
