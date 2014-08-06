1b0519990be90c1422deacda43276f5e82d5aaf4
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-06 23:53:39 +0600

    Added Meeting's model validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,22 @@
 class Meeting < ActiveRecord::Base
+
+  OPENED = 1    #Открыт
+  CLOSED = 2    #Закрыт
+
   belongs_to              :owner, class_name: User
   belongs_to              :project
   has_and_belongs_to_many :users
+
+  # Статусы
+  extend Enumerize
+  enumerize :status, in: {
+      'открыт' => self::OPENED,
+      'закрыт' => self::CLOSED,
+  }
+
+  # Валидация
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :owner, presence: true
+  validates :project, presence: true
+  validates :status, presence: true, inclusion: { in: self.status.values }
 end
--- spec/factories/meetings.rb
+++ spec/factories/meetings.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :meeting do
     title { Faker::Lorem.characters(rand(4..30)) }
-    status 1
+    status 'открыт'
 
     association :project, factory: :project
     association :owner, factory: :user
