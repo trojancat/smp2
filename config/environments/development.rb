0cc91cc6439a8d213d33c6cf8f227d427feda10b
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-11 18:05:13 +0600

    Added reset password function

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,11 +41,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: ENV["GMAIL_DOMAIN"],
+      domain: 'gmail.com',
       authentication: "plain",
       enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
+      user_name: 'bloomingcoast@gmail.com',
+      password: 'yflt;lf25'
   }
 
 
