b839b679c7e8c8234e6acae757234ad49d589f61
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-08-13 14:21:37 +0600

    Added twitter bootstrap 3 theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
+//= require plugins/metisMenu/metisMenu.min
+//= require sb-admin-2
 //= require turbolinks
 //= require_tree .
